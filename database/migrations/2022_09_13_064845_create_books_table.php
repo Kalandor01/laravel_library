8ac10d2bd089ebe956be97ea320423185c52894d
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-09-13 10:03:27 +0200

    database

---
+++ database/migrations/2022_09_13_064845_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Book;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('author', 32);
+            $table->string('title', 150);
+            $table->integer('pieces')->default(10);
+            $table->timestamps();
+        });
+
+        Book::create(['author'=>'Cooleen Hoovel', 'title'=>'It ends with us']);
+        Book::create(['author'=>'Lovecraft', 'title'=>'The call of Cthulhul', 'pieces'=>120]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
